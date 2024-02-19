c9087ff933e30291d118c7d55153e674106d4457
masterkjellevold <syvertkt@stud.ntnu.no>
2024-02-19 13:59:52 +0100

    commenting out lamp in main

--- lab_2/skeleton_project/source/main.c
+++ lab_2/skeleton_project/source/main.c
@@ -25,13 +25,14 @@ int main(){
             elevio_motorDirection(DIRN_DOWN);
         }
 
-
+        /*
         for(int f = 0; f < N_FLOORS; f++){
             for(int b = 0; b < N_BUTTONS; b++){
                 int btnPressed = elevio_callButton(f, b);
                 elevio_buttonLamp(f, b, btnPressed);
             }
         }
+        */
 
         if(elevio_obstruction()){
             elevio_stopLamp(1);
