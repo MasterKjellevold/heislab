bbe54fb4b089df742c9c11bf29b3ae0ab074b94c
JohannesBrenn <johabren@stud.ntnu.no>
2024-02-19 14:38:36 +0100

    modified button.c

--- lab_2/skeleton_project/source/button.c
+++ lab_2/skeleton_project/source/button.c
@@ -6,28 +6,28 @@ Button buttons[];
 void init_button(){
 
     // initialize buttons in first floor
-    Button *firstFloorUp = {firstFloorUp.floor = 1, firstFloorUp.buttonType = BUTTON_HALL_UP};
-    Button *firstFloorCab = {firstFloorUp.floor = 1, firstFloorUp.buttonType = BUTTON_CAB}
+    Button *firstFloorUp = {firstFloorUp->floor = 1, firstFloorUp->buttonType = BUTTON_HALL_UP};
+    Button *firstFloorCab = {firstFloorCab->floor = 1, firstFloorCab->buttonType = BUTTON_CAB};
 
     // initalize buttons
     int count = 0;
     for(int f = 1; f < N_FLOORS - 2 ; f++){
-        Button *newUp = {new.floor = f, new.buttonType = BUTTON_HALL_UP};
-        Button *newDown = {new.floor = f, new.buttonType = BUTTON_HALL_DOWN};
-        Button *newCab = {new.floor = f, new.buttonType = BUTTON_CAB};
+        Button *newUp = {newUp->floor = f, newUp->buttonType = BUTTON_HALL_UP};
+        Button *newDown = {newDown->floor = f, newDown->buttonType = BUTTON_HALL_DOWN};
+        Button *newCab = {newCab->floor = f, newCab->buttonType = BUTTON_CAB};
 
         button[count] = newUp;
         button[count + 1] = newDown;
-        button[count + 2] = new
+        button[count + 2] = newCab;
         count += 3;
     }
     // initialize buttons in last floor
-    Button *lastFloorUp = {lastFloorUp.floor = N_FLOORs, lastFloorUp.buttonType = BUTTON_HALL_UP};
-    Button *lastFloorCab = {lastFloorUp.floor = N_FLOORS, lastFloorUp.buttonType = BUTTON_CAB}
+    Button *lastFloorUp = {lastFloorUp->floor = N_FLOORS, lastFloorUp->buttonType = BUTTON_HALL_UP};
+    Button *lastFloorCab = {lastFloorCab->floor = N_FLOORS, lastFloorCab->buttonType = BUTTON_CAB};
 }
 
-void loop_through(){
-
+int loop_through(){
+    return 0;
 }
 
 
