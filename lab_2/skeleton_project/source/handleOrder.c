ea196043bd76de1493f9de74b175442323b943a8
JohannesBrenn <johabren@stud.ntnu.no>
2024-02-26 15:18:12 +0100

    Fikset handleOrder slik at den funker

--- lab_2/skeleton_project/source/handleOrder.c
+++ lab_2/skeleton_project/source/handleOrder.c
@@ -1,12 +1,22 @@
 
-
 void handleOrdre(OrderList **head, Order newOrder) {
+
+    //Checking if the order list is empty, and adding ordre directly
+    if(*head == NULL) {
+        addToQueue(head, newOrder);
+        return;
+    }
 
     //Checking if newOrder is already in the order list
     int newOrderNotInList = 1;
-    while(head->next != NULL) {
-        if( ((*head)->order.floor == newOrder.floor) && ((*head)->order.btype == newOrder.btype) ) {
+    OrderList *current = *head;
+    while(current->next != NULL) {
+        if( (current->order.floor == newOrder.floor) && (current->order.btype == newOrder.btype) ) {
             newOrderNotInList = 0;
+            break;
+        }
+        else {
+            current = current->next;
         }
     }
     
